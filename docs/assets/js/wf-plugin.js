c520ffff193aefab46d1dafab1306658f8e91269
jeferson <winterjeferson@gmail.com>
2022-08-24 08:36:48 -0300

    responsive fix

--- docs/assets/js/wf-plugin.js
+++ docs/assets/js/wf-plugin.js
@@ -388,12 +388,63 @@ export class LoadingMain {
         this.elBody.style.overflow = 'auto';
     }
 }
+export class MenuToggle {
+    constructor() {
+        this.cssButton = 'toggle-menu';
+        this.cssOpen = 'toggle-menu__open';
+        this.isWatch = false;
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    buildMenu() {
+        this.elButton.forEach((el) => {
+            helper.addClick(el, this.handleClick);
+        });
+    }
+
+    handleClick(args) {
+        const elSibling = args.target.nextElementSibling;
+        const isCss = elSibling.classList.contains(this.cssOpen);
+
+        if (isCss) {
+            elSibling.classList.remove(this.cssOpen);
+            return;
+        }
+        elSibling.classList.add(this.cssOpen);
+    }
+
+    init() {
+        this.update();
+        this.buildMenu();
+
+        if (!this.isWatch) {
+            this.isWatch = true;
+            this.watchResize();
+        }
+    }
+
+    update() {
+        this.elButton = document.querySelectorAll(`.${this.cssButton}`);
+    }
+
+    reset() {
+        this.init();
+    }
+
+    watchResize() {
+        window.onresize = () => {
+            this.init();
+        };
+    }
+}
 window.loadingMain = new LoadingMain();
 window.carousel = new Carousel();
 window.helper = new Helper();
+window.menuToggle = new MenuToggle();
 
 window.addEventListener('load',
     window.carousel.init(),
+    window.menuToggle.init(),
     window.loadingMain.hide(), {
         once: true
     });
--- src/js/wf-plugin/index.js
+++ src/js/wf-plugin/index.js
@@ -1,9 +1,11 @@
 window.loadingMain = new LoadingMain();
 window.carousel = new Carousel();
 window.helper = new Helper();
+window.menuToggle = new MenuToggle();
 
 window.addEventListener('load',
     window.carousel.init(),
+    window.menuToggle.init(),
     window.loadingMain.hide(), {
         once: true
     });
